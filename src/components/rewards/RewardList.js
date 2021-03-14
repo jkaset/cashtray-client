fce8a8289cad34b215b8617aaacde88e2c381d79
Jasmin Kaset <kasetski@gmail.com>
2021-03-14 18:38:03 -0500

    reward state works

--- src/components/rewards/RewardList.js
+++ src/components/rewards/RewardList.js
@@ -4,18 +4,37 @@ import { RewardContext } from "./RewardProvider"
 
 export const RewardList = () => {
 
-  const { rewards, getRewards, redeemReward, deleteReward, available, setAvailable } = useContext(RewardContext)
+  const { rewards, getRewards, redeemReward, deleteReward } = useContext(RewardContext)
   const { singleNonsmoker, getSingleNonsmoker } = useContext(NonsmokerContext)
- 
-
+  
   useEffect(() => {
     getSingleNonsmoker()
       .then(getRewards)
-       
-  }, [])
+      
+    }, [])
+    
+  const [spentCashAmount, setSpentCashAmount] = useState(0)
+    
+  useEffect(() => {
+    
+      let redeemedRewardsArray = []
+      const reducer = (accumulator, currentValue) => accumulator + currentValue
+      rewards.forEach(r=> {
+        if (r.redeemed === true) {
+          redeemedRewardsArray.push(r.reward_cost)
+        }
+        console.log(redeemedRewardsArray)
+        const redeemedTotal = redeemedRewardsArray.reduce(reducer)
+        setSpentCashAmount(redeemedTotal)
+      })
+    }, [rewards])
 
+  const [availableCashAmount, setAvailableCashAmount] = useState(0)
 
-
+  useEffect(() => {
+      const newTotal = allTimeTotal - spentCashAmount
+      setAvailableCashAmount(newTotal)
+  }, [rewards])
 
 
   const daily_cost = ((singleNonsmoker.cigs_per_day) / (singleNonsmoker.cigs_per_pack)) * (singleNonsmoker.price_per_pack)
@@ -33,21 +52,22 @@ export const RewardList = () => {
   //   }
 
   //this function "works" but needs to change state
-  let spentCashTotal = 0 
-  const spentCash = 
-    rewards.map((reward) => {
-      if (reward.redeemed === true) {
-        return spentCashTotal += parseInt(reward.reward_cost)
-      }
-    })
-  // console.log(spentCashTotal)
-  const availableCash = 
-    rewards.map((reward) => {
-      if (reward.redeemed === true) {
-        return allTimeTotal - spentCashTotal
-      }
+
+  // const spentCash = 
+  //   rewards.map((reward) => {
+  //     if (reward.redeemed === true) {
+  //       const spent = reduce(reward.reward_cost)
+  //       setSpentCashAmount(spent)
+  //     }
+  //   })
+  // console.log(spentCashAmount)
+  // const availableCash = 
+  //   rewards.map((reward) => {
+  //     if (reward.redeemed === true) {
+  //       return allTimeTotal - spentCashTotal
+  //     }
      
-    }) 
+  //   }) 
   
 
 
@@ -55,10 +75,8 @@ export const RewardList = () => {
     <>
       <p>my rewards</p>
       All-time savings: {allTimeTotal}
-      <p>Total Spent: {spentCash}</p>
-      <p>Available cash: {availableCash}
-
-      </p>
+      <p>Total Spent: {spentCashAmount}</p>
+      <p>Available cash: {availableCashAmount}</p>
       <ul>
         {rewards.map((reward) => {
 
@@ -68,7 +86,7 @@ export const RewardList = () => {
               onClick={event => {
                 event.preventDefault()
                 redeemReward(reward)
-                // .then(minusRedeemedValues)
+                
               }}> redeem
 
             </button>
