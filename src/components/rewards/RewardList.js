5d50a441621e613a19d3a86c1b1a343d524d09fa
Jasmin Kaset <kasetski@gmail.com>
2021-03-15 06:58:20 -0500

    branch stuck

--- src/components/rewards/RewardList.js
+++ src/components/rewards/RewardList.js
@@ -1,42 +1,44 @@
 import React, { useContext, useEffect, useState } from "react"
 import { NonsmokerContext } from "../nonsmokers/NonsmokerProvider"
 import { RewardContext } from "./RewardProvider"
+import "./Reward.css"
 
 export const RewardList = () => {
 
   const { rewards, getRewards, redeemReward, deleteReward } = useContext(RewardContext)
   const { singleNonsmoker, getSingleNonsmoker } = useContext(NonsmokerContext)
-  
+  const [spentCashAmount, setSpentCashAmount] = useState(0)
+  const [availableCashAmount, setAvailableCashAmount] = useState()
+
+  const [isRedeemed, setRedeemed] = useState("false")
+
+  const handleToggle = () => {
+    setRedeemed(!isRedeemed)
+  }
+
+
   useEffect(() => {
     getSingleNonsmoker()
       .then(getRewards)
+  }, [])
 
-      
-    }, [])
-
-
-    
-  const [spentCashAmount, setSpentCashAmount] = useState(0)
-    
-  useEffect(() => {
-    
-      let redeemedRewardsArray = [0]
-      const reducer = (accumulator, currentValue) => accumulator + currentValue
-      rewards.forEach(r=> {
-        if (r.redeemed === true) {
-          redeemedRewardsArray.push(r.reward_cost)
-        }
-        console.log(redeemedRewardsArray)
-        const redeemedTotal = redeemedRewardsArray.reduce(reducer)
-        setSpentCashAmount(redeemedTotal)
-      })
-    }, [rewards])
-
-  const [availableCashAmount, setAvailableCashAmount] = useState(0)
 
   useEffect(() => {
-      const newTotal = allTimeTotal - spentCashAmount
-      setAvailableCashAmount(newTotal)
+    const newTotal = allTimeTotal - spentCashAmount
+    setAvailableCashAmount(newTotal)
+  }, [rewards])
+
+  useEffect(() => {
+    let redeemedRewardsArray = [0]
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    rewards.forEach(r => {
+      if (r.redeemed === true) {
+        redeemedRewardsArray.push(r.reward_cost)
+      }
+      // console.log(redeemedRewardsArray)
+      const redeemedTotal = redeemedRewardsArray.reduce(reducer)
+      setSpentCashAmount(redeemedTotal)
+    })
   }, [rewards])
 
 
@@ -44,41 +46,39 @@ export const RewardList = () => {
 
   const allTimeTotal = (singleNonsmoker.time_smoke_free) * daily_cost
 
-  console.log(singleNonsmoker.time_smoke_free)
-  console.log(allTimeTotal)
-
-  
-
 
   return (
     <>
-      <p>my rewards</p>
+      <p>My Rewards</p>
       All-time savings: {allTimeTotal}
-      <p>Total Spent: {spentCashAmount}</p>
       <p>Available cash: {availableCashAmount}</p>
+      <p>Total Spent: {spentCashAmount}</p>
       <ul>
         {rewards.map((reward) => {
 
-          return <li>
+          return <li key={reward.id} className={isRedeemed ? "app" : "danger"}>
             {reward.reward_name} : {reward.reward_cost}
-            <button type="submit"
+            <button type="submit" className="button_on"
               onClick={event => {
                 event.preventDefault()
+               
                 redeemReward(reward)
-                
+              
+               
+
               }}> redeem
 
             </button>
-            <button type="submit"
+            <button type="submit" className="button_on"
               onClick={event => {
                 event.preventDefault()
                 deleteReward(reward)
-                
+
               }}> delete
 
             </button>
           </li>
-          //  }
+
         })}
       </ul>
 
