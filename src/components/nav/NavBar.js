9e80407478621a9e34ad817c9f4005450df2fa34
Jasmin Kaset <kasetski@gmail.com>
2021-03-17 16:19:48 -0500

    container, jumbotrons at /

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from "react-router-dom"
 import "./NavBar.css"
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 
 export const NavBar = () => {
@@ -10,21 +11,22 @@ export const NavBar = () => {
 
   return (
     <>
-    <Nav>
-    <Nav.Item>
-    <Nav.Link href="/">Home</Nav.Link>
-    </Nav.Item>
+      <Container>
+        <Nav>
+          <Nav.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav.Item>
 
-    <Nav.Item>
-    <Nav.Link href="/wallet">Wallet</Nav.Link>
-    </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/wallet">Wallet</Nav.Link>
+          </Nav.Item>
 
-    <Nav.Item>
-    <Nav.Link href="/community">Community</Nav.Link>
-    </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/community">Community</Nav.Link>
+          </Nav.Item>
 
 
-{/* 
+          {/* 
       <li className="navbar__item">
         <Link className="navbar__link" to="/">Home</Link>
       </li>
@@ -34,29 +36,30 @@ export const NavBar = () => {
       <li className="navbar__item">
         <Link className="navbar__link" to="/community">Community</Link>
       </li> */}
-    <Button>
-      {
-        (localStorage.getItem("cashtray_token") !== null) ?
-          <li className="nav-item">
-            <button className="nav-link fakeLink"
-              onClick={() => {
-                localStorage.removeItem("cashtray_token")
-                history.push({ pathname: "/" })
-              }}
-            >Logout</button>
-          </li> :
-          <>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/register">Register</Link>
-            </li>
-          </>
-      }
-    </Button>
-    {/* </ul> */}
-    </Nav>
-      </>
+
+          {
+            (localStorage.getItem("cashtray_token") !== null) ?
+              <li className="nav-item">
+                <Button className="nav-link fakeLink"
+                  onClick={() => {
+                    localStorage.removeItem("cashtray_token")
+                    history.push({ pathname: "/" })
+                  }}
+                >Logout</Button>
+              </li> :
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">Register</Link>
+                </li>
+              </>
+          }
+
+          {/* </ul> */}
+        </Nav>
+      </Container>
+    </>
   )
 }
